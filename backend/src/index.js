9ae896ba7fb39588c1f1abb8fe3b50b3c8b7e0a9
Prabhat Kumar <prabhatmathur9927@gmail.com>
2025-08-16 15:22:57 +0530

    added some modifications

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
-import express from "express"
-import dotenv from "dotenv"
-import { connectDB } from "./lib/db.js"
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
-import cookieParser from 'cookie-parser'
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import path from 'path';
 
 dotenv.config()
 
@@ -11,11 +13,21 @@ const app=express()
 
 const PORT=process.env.PORT
 
-app.use(express.json())
+app.use(express.json({ limit: "10mb" }))
 app.use(cookieParser())
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}))
 
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 app.listen(PORT,()=>{
     console.log(`server is ready at ${PORT}`)
